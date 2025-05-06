e3b811d577fc2ab99a5c595ae487333311c12795
Po-Chun Wu <pcwu2022@gmail.com>
2025-05-06 14:45:31 +0800

    Created Heap

--- kuos_algorithm/kuo.py
+++ kuos_algorithm/kuo.py
@@ -1,13 +1,13 @@
 import networkx as nx
 
-G = nx.Graph()
-
 # Read File
 input_filename = "./data/sample.in.txt"
 
 with open(input_filename, "r") as file:
     input_edges = [(line.split(' ')[0], line.split(' ')[1]) for line in file.read().splitlines()]
 
+# Create Graph
+G = nx.DiGraph(input_edges)
 
-
+print(G._node)
 

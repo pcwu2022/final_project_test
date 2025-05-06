5516ad63c152d71eadb805f75dcf3522c8167ceb
Po-Chun Wu <pcwu2022@gmail.com>
2025-05-06 14:45:44 +0800

    Created Heap

--- kuos_algorithm/kuo.py
+++ kuos_algorithm/kuo.py
@@ -1,13 +1,23 @@
 import networkx as nx
+import heapq
 
-# Read File
-input_filename = "./data/sample.in.txt"
+def get_graph_from_file(filename):
+    # Read File
 
-with open(input_filename, "r") as file:
-    input_edges = [(line.split(' ')[0], line.split(' ')[1]) for line in file.read().splitlines()]
+    with open(filename, "r") as file:
+        input_edges = [(line.split(' ')[0], line.split(' ')[1]) for line in file.read().splitlines()]
 
-# Create Graph
-G = nx.DiGraph(input_edges)
+    # Create Graph
+    return nx.DiGraph(input_edges)
 
-print(G._node)
+def kuos_algorithm(G):
+    # Create Heap for storing the elements
+    heap = []
+    for node, degree in G.in_degree():
+        heapq.heappush(heap, (degree, node))
+
+# Run Algorithm
+if __name__ == "__main__":
+    G = get_graph_from_file("./data/sample.in.txt")
+    kuos_algorithm(G)
 

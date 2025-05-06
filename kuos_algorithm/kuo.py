ba988411ce4fd593bb6842c9b93635d15fc60766
Po-Chun Wu <pcwu2022@gmail.com>
2025-05-06 14:24:42 +0800

    Added Read File

--- kuos_algorithm/kuo.py
+++ kuos_algorithm/kuo.py
@@ -1,1 +1,13 @@
-print("Hello, World!")
+import networkx as nx
+
+G = nx.Graph()
+
+# Read File
+input_filename = "./data/sample.in.txt"
+
+with open(input_filename, "r") as file:
+    input_edges = [(line.split(' ')[0], line.split(' ')[1]) for line in file.read().splitlines()]
+
+
+
+
